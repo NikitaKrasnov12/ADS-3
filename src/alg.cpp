819aaf80ea04267b0c62a3710a4cfb38d405b115
NikitaKrasnov12 <116708902+NikitaKrasnov12@users.noreply.github.com>
2023-03-25 15:04:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,29 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int start = 0;
+    int end = size - 1;
+    int count = 0;
+    while (start <= end) {
+        int half = (start + end) / 2;
+        if (arr[half] == value) {
+            count++;
+            int i = half - 1;
+            while (i >= start && arr[i] == value) {
+                count++;
+                i--;
+            }
+            int k = half + 1;
+            while (k <= end && arr[k] == value) {
+                count++;
+                k++;
+            }
+            return count;
+        } else if (arr[half] < value) {
+            start = half + 1;
+        } else {
+            end = half - 1;
+        }
+    }
+    return 0;
 }
